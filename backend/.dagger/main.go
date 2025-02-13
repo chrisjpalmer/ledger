f93140e458e1f107d798e2048eebfd4d0572f25d
chrisjpalmer <chrisjpalmer@acme.com>
2025-02-14 08:39:20 +1100

    🔥 Remove uneeded dagger functions
    This commit removes some of the uneeded
    dagger functions that were created when
    dagger was first initialised.

--- backend/.dagger/main.go
+++ backend/.dagger/main.go
@@ -21,21 +21,6 @@ import (
 
 type Backend struct{}
 
-// Returns a container that echoes whatever string argument is provided
-func (m *Backend) ContainerEcho(stringArg string) *dagger.Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
-}
-
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *Backend) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
-}
-
 func (m *Backend) PostgresMigrate(ctx context.Context, src *dagger.Directory) (*dagger.Service, error) {
 	pg, err := m.Postgres().Start(ctx)
 	if err != nil {
