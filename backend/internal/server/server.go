e6752e1faf59fd88120fcdea03501d42c55efc60
chrisjpalmer <chrisjpalmer@acme.com>
2025-02-21 08:06:07 +1100

    ✨ Allow income lines to be added to ledger
    This commit allows income lines to be added to
    a specific month of the ledger. This will enable future
    users to create new income lines, to track their
    cash flow using the ledger app.

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -3,9 +3,11 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	openapi "github.com/chrisjpalmer/ledger/backend/internal/api/go"
 	"github.com/chrisjpalmer/ledger/backend/internal/postgres"
+	"github.com/gorilla/mux"
 	"go.uber.org/zap"
 )
 
@@ -30,11 +32,45 @@ func NewServer(zl *zap.Logger, postgres *postgres.Postgres, c Config) *Server {
 	// configure server
 	ctl := openapi.NewLedgerAPIController(&srv)
 	srv.httpSrv = &http.Server{
-		Handler: openapi.NewRouter(ctl),
+		Handler: newRouter(zl, ctl),
 		Addr:    fmt.Sprintf(":%d", c.Port),
 	}
 
 	return &srv
+}
+
+// newRouter - wire the openapi generated code to gorilla mux
+func newRouter(zl *zap.Logger, api openapi.Router) *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	for name, route := range api.Routes() {
+		var handler http.Handler = route.HandlerFunc
+		handler = logger(zl, handler, name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(name).
+			Handler(handler)
+	}
+
+	return router
+}
+
+func logger(zl *zap.Logger, inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		inner.ServeHTTP(w, r)
+
+		zl.Info(
+			"request handled",
+			zap.String("method", r.Method),
+			zap.String("request_uri", r.RequestURI),
+			zap.String("name", name),
+			zap.Duration("duration", time.Since(start)),
+		)
+	})
 }
 
 func (s *Server) Listen() error {
--- backend/sql/V1__initial.sql
+++ backend/sql/V1__initial.sql
@@ -1,3 +1,10 @@
-CREATE TABLE MyTable (
-    MyColumn VARCHAR(100) NOT NULL
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE income (
+    amount FLOAT NOT NULL,
+    "date" DATE NOT NULL,
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "month" INTEGER NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "received" BOOLEAN NOT NULL
 );
