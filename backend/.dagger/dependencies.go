2a7e9f14095a950853bf5a7814024d9d043cd86f
chrisjpalmer <chrisjpalmer@acme.com>
2025-02-24 08:07:54 +1100

    👷 Add check-pull-request check
    This commit adds a check to be run on every pull request.
    The purpose is to build and run the code to validate it is
    working as expected. There are no tests as of yet, however
    in the future, tests will also be run as part of this check.

---
+++ backend/.dagger/dependencies.go
@@ -0,0 +1,79 @@
+// A generated module for Backend functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+	"dagger/backend/internal/dagger"
+)
+
+type Backend struct{}
+
+// PostgresMigrate - spins up a postgres database and runs the migrations against it.
+func (m *Backend) PostgresMigrate(ctx context.Context, src *dagger.Directory) (*dagger.Service, error) {
+	pg, err := m.Postgres().Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = m.Migrate(ctx, src, pg)
+	if err != nil {
+		return nil, err
+	}
+
+	return pg, nil
+}
+
+// Postgres - creates a new postgres database
+func (m *Backend) Postgres() *dagger.Service {
+	return dag.Container().
+		From("postgres:17.2-bookworm").
+		WithEnvVariable("POSTGRES_PASSWORD", "password").
+		WithExposedPort(5432).
+		AsService()
+}
+
+// Migrate - migrates a postgres database.
+// `src` is the directory of the backend project.
+// `svc` is the database endpoint.
+func (m *Backend) Migrate(ctx context.Context, src *dagger.Directory, svc *dagger.Service) (string, error) {
+	return dag.Container().
+		From("flyway/flyway").
+		WithMountedDirectory("/flyway/project", src).
+		WithServiceBinding("db", svc).
+		WithExec([]string{
+			"-url=jdbc:postgresql://db:5432/postgres?user=postgres&password=password",
+			"-workingDirectory=project",
+			"migrate",
+		}, dagger.ContainerWithExecOpts{UseEntrypoint: true}).
+		Stdout(ctx)
+}
+
+// OpenapiGenerate - generates go server boilerplate code from the openapi spec.
+// `src` is the directory of the backend project.
+func (m *Backend) OpenapiGenerate(ctx context.Context, src *dagger.Directory) *dagger.Directory {
+	return dag.Container().
+		From("openapitools/openapi-generator-cli").
+		WithDirectory("/local", src.Directory("api")).
+		WithDirectory("/out", src.Directory("internal/api")).
+		WithExec([]string{
+			"generate",
+			"-i", "/local/spec.yaml",
+			"-g", "go-server",
+			"-p", "outputAsLibrary=true",
+			"-o", "/out",
+		}, dagger.ContainerWithExecOpts{UseEntrypoint: true}).
+		Directory("/out")
+}
